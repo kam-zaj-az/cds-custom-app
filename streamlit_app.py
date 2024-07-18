5ff2263266f0422f66108798b8f324f0fc623677
kam-zaj-az <kamil.zajecki.az@gmail.com>
2024-07-18 13:46:51 +0200

    Adding templates.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,40 @@ import openpyxl
 
 def main():
     st.title("Clinical Data Standards Web Service")
-    st.write("This is a main screen of app to get the info of tools")
+    st.write("This is a main screen of app to get the info of tools:")
+    st.slider('x')
+    st.write(x, 'squared is', x * x)
     st.subheader("This is a subheader")
     st.text("This is a st.text function.")
 
+    st.text_input("Your name", key="name")
+
+    # You can access the value at any point with:
+    st.session_state.name
+    if st.checkbox('Show entered name'):
+        st.write(st.session_state.name)
+
+    df = pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+        })
+
+    option = st.selectbox(
+        'Which number do you like best?',
+        df['first column'])
+
+    'You selected: ', option
+    # Add a selectbox to the sidebar:
+    add_selectbox = st.sidebar.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone')
+    )
+
+    # Add a slider to the sidebar:
+    add_slider = st.sidebar.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0)
+    )
 
 
 

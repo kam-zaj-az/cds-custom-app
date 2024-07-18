a620723ac8177f8b731f8920ed4039d3c1e77e4e
kam-zaj-az <kamil.zajecki.az@gmail.com>
2024-07-18 13:29:00 +0200

    Trying multipage app

---
+++ pages/RDS correction.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+
+def main():
+    st.title("Excel File Uploader and Viewer")
+
+    uploaded_file = st.file_uploader("Upload Files", type=['xlsx', 'xls'], key=11)
+    if uploaded_file is not None:
+        # Process the uploaded file here
+        # (e.g., save it, analyze it, etc.)
+        st.write("File uploaded successfully!")
+        df1 = pd.read_excel(uploaded_file)
+        st.write(df1)
+
+if __name__ == "__main__":
+    main()

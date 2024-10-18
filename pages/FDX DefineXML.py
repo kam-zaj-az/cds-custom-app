808ad852970706cbfbbfcfa20ad9419d5d3b2504
kam-zaj-az <kamil.zajecki.az@gmail.com>
2024-10-18 16:15:46 +0200

    test 2

--- pages/FDX DefineXML.py
+++ pages/FDX DefineXML.py
@@ -39,9 +39,9 @@ def main():
         with tab1:
             st.header("Datasets")
 
-            df = pd.DataFrame.from_dict(pd.json_normalize(json_content), orient='columns')
+            #df = pd.DataFrame.from_dict(pd.json_normalize(json_content), orient='columns')
 
-            st.dataframe(df)
+            #st.dataframe(df)
 
         with tab2:
             st.header("Variables")

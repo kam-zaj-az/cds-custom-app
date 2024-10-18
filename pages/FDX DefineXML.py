c51b1f533a6942320a55a8fc3635e591ef105958
kam-zaj-az <kamil.zajecki.az@gmail.com>
2024-10-18 16:51:15 +0200

    fix

--- pages/FDX DefineXML.py
+++ pages/FDX DefineXML.py
@@ -39,7 +39,7 @@ def main():
         with tab_main:
             st.header("Global variables")
             data = {}
-            data['FileOID'] = json_content["{http://www.cdisc.org/ns/odm/v1.3}ODM"]["FileOID"]
+            data['FileOID'] = json_content["ODM"]["FileOID"]
             st.write(data)
 
             #df = pd.DataFrame.from_dict(data)

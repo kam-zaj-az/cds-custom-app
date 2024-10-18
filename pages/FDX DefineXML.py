d139f7cb2d1b196dc218bfe6fce84d0a63b95e9a
kam-zaj-az <kamil.zajecki.az@gmail.com>
2024-10-18 15:29:32 +0200

    Test 5

--- pages/FDX DefineXML.py
+++ pages/FDX DefineXML.py
@@ -12,43 +12,43 @@ def main():
     if uploaded_file is not None:
         st.write("File uploaded successfully!")
 
-        json_bytes = xml_file_to_json_bytes(uploaded_file)
+        content = uploaded_file.read()
 
         st.download_button(
             label="Download JSON",
-            data=file1_data,
+            data=content,
             file_name="output.json",
             mime="application/json"
         )
 
-def xml_to_dict(element):
-    # Initialize the dictionary with attributes
-    result = {k: v for k, v in element.attrib.items()}
+# def xml_to_dict(element):
+#     # Initialize the dictionary with attributes
+#     result = {k: v for k, v in element.attrib.items()}
     
-    # If the element has no children, just set its text
-    if len(element) == 0:
-        if element.text:
-            result['#text'] = element.text
-        return result
+#     # If the element has no children, just set its text
+#     if len(element) == 0:
+#         if element.text:
+#             result['#text'] = element.text
+#         return result
     
-    # Otherwise, iterate over children and recursively convert them
-    for child in element:
-        child_result = xml_to_dict(child)
-        if child.tag in result:
-            if not isinstance(result[child.tag], list):
-                result[child.tag] = [result[child.tag]]
-            result[child.tag].append(child_result)
-        else:
-            result[child.tag] = child_result
+#     # Otherwise, iterate over children and recursively convert them
+#     for child in element:
+#         child_result = xml_to_dict(child)
+#         if child.tag in result:
+#             if not isinstance(result[child.tag], list):
+#                 result[child.tag] = [result[child.tag]]
+#             result[child.tag].append(child_result)
+#         else:
+#             result[child.tag] = child_result
     
-    return result
+#     return result
 
-def xml_file_to_json_bytes(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    xml_dict = {root.tag: xml_to_dict(root)}
-    json_str = json.dumps(xml_dict, ensure_ascii=False, indent=4)
-    return BytesIO(json_str.encode('utf-8'))
+# def xml_file_to_json_bytes(xml_file):
+#     tree = ET.parse(xml_file)
+#     root = tree.getroot()
+#     xml_dict = {root.tag: xml_to_dict(root)}
+#     json_str = json.dumps(xml_dict, ensure_ascii=False, indent=4)
+#     return BytesIO(json_str.encode('utf-8'))
 
 if __name__ == "__main__":
     main()
